0b1d1175e3401d441fb167b5b721bf74eace31be
Gavin Zhang <zxb_worky@163.com>
2023-03-17 23:26:14 +0800

    添加-短链内部处理责任链雏形

--- armagin-server/src/main/java/cc/forim/armagin/server/action/TransformEventAction.java
+++ armagin-server/src/main/java/cc/forim/armagin/server/action/TransformEventAction.java
@@ -2,6 +2,9 @@ package cc.forim.armagin.server.action;
 
 import cc.forim.armagin.server.pipeline.BusinessProcess;
 import cc.forim.armagin.server.pipeline.TransformContext;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
 
 /**
  * 【4】转换事件发送流程
@@ -10,6 +13,9 @@ import cc.forim.armagin.server.pipeline.TransformContext;
  * @version V1.0
  * @since 2023/3/17 23:02
  */
+
+@Component
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class TransformEventAction implements BusinessProcess {
     @Override
     public void process(TransformContext context) {
