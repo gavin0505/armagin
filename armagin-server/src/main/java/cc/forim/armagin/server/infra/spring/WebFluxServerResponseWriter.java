c769eb1b2e82d6a62dcec63a4dbf817701ba4960
Gavin Zhang <zxb_worky@163.com>
2023-03-19 14:53:23 +0800

    增加-重定向到自定义页面

--- armagin-server/src/main/java/cc/forim/armagin/server/infra/spring/WebFluxServerResponseWriter.java
+++ armagin-server/src/main/java/cc/forim/armagin/server/infra/spring/WebFluxServerResponseWriter.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
 
 import java.net.URI;
 
@@ -20,6 +21,12 @@ import static org.springframework.web.cors.CorsConfiguration.ALL;
 @Component
 public class WebFluxServerResponseWriter {
 
+    /**
+     * 真正执行重定向的任务
+     *
+     * @param exchange 请求
+     * @param url      重定向的目标url
+     */
     public Runnable redirectAction(ServerWebExchange exchange, String url) {
         return () -> {
             ServerHttpResponse response = exchange.getResponse();
@@ -29,5 +36,24 @@ public class WebFluxServerResponseWriter {
             response.getHeaders().set(HttpHeaders.ACCESS_CONTROL_ALLOW_HEADERS, ALL);
             response.getHeaders().set(HttpHeaders.ACCESS_CONTROL_ALLOW_CREDENTIALS, Boolean.TRUE.toString());
         };
+    }
+
+    /**
+     * 重定向到自定义页面
+     *
+     * @param exchange 请求
+     * @param url      重定向目标url
+     */
+    public Mono<Void> redirect(ServerWebExchange exchange, String url) {
+        ServerHttpResponse response = exchange.getResponse();
+        if (!response.isCommitted()) {
+            response.setStatusCode(HttpStatus.FOUND);
+            response.getHeaders().setLocation(URI.create(url));
+            response.getHeaders().set(HttpHeaders.ACCESS_CONTROL_ALLOW_HEADERS, ALL);
+            response.getHeaders().set(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, ALL);
+            response.getHeaders().set(HttpHeaders.ACCESS_CONTROL_ALLOW_CREDENTIALS, Boolean.TRUE.toString());
+            return response.setComplete();
+        }
+        return Mono.empty();
     }
 }
