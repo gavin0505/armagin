503f0dd60c8783c2a24aef7037b2378c6d34afcc
Gavin Zhang <zxb_worky@163.com>
2023-03-19 12:28:06 +0800

    增加-短链转换状态枚举

---
+++ armagin-server/src/main/java/cc/forim/armagin/server/enums/TransformStatus.java
@@ -0,0 +1,42 @@
+package cc.forim.armagin.server.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+/**
+ * 短链转换状态
+ *
+ * @author Gavin Zhang
+ * @version V1.0
+ * @since 2023/3/18 23:18
+ */
+
+@Getter
+@ToString
+@AllArgsConstructor
+public enum TransformStatus {
+
+    /**
+     * 转换成功
+     */
+    TRANSFORM_SUCCESS(1),
+
+    /**
+     * 转换失败
+     */
+    TRANSFORM_FAIL(2),
+
+    /**
+     * 重定向成功
+     */
+    REDIRECTION_SUCCESS(3),
+
+    /**
+     * 重定向失败
+     */
+    REDIRECTION_FAIL(4),
+
+    ;
+    private final Integer code;
+}
