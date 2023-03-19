df6610233824709c72eb08aab6d8749327daea44
Gavin Zhang <zxb_worky@163.com>
2023-03-19 14:46:40 +0800

    增加-Redis缓存键枚举

---
+++ armagin-server/src/main/java/cc/forim/armagin/server/enums/CacheKeyEnum.java
@@ -0,0 +1,25 @@
+package cc.forim.armagin.server.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+/**
+ * Redis缓存键枚举
+ *
+ * @author Gavin Zhang
+ * @version V1.0
+ * @since 2023/3/19 13:25
+ */
+
+@Getter
+@ToString
+@AllArgsConstructor
+public enum CacheKeyEnum {
+
+    /**
+     * 映射前缀
+     */
+    UM("armagin:server:um:");
+    private final String key;
+}
