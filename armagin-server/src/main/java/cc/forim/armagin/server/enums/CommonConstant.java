0c88d6bd5a17def1621f52d55dd00785a737d8f9
Gavin Zhang <zxb_worky@163.com>
2023-03-19 14:47:12 +0800

    增加-协议通用常量类

---
+++ armagin-server/src/main/java/cc/forim/armagin/server/enums/CommonConstant.java
@@ -0,0 +1,19 @@
+package cc.forim.armagin.server.enums;
+
+/**
+ * 协议通用常量
+ *
+ * @author Gavin Zhang
+ * @version V1.0
+ * @since 2023/3/19 13:38
+ */
+public class CommonConstant {
+
+    public static final String HTTPS_PROTOCOL = "https";
+
+    public static final String HTTP_PROTOCOL = "http";
+
+    public static final String PROTOCOL_SUFFIX = "://";
+
+    public static final String PROTOCOL_SP = "/";
+}
