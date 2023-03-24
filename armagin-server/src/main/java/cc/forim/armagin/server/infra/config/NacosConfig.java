6a5a8aad4696245f25d8ef4c5d62b0b549b54a0e
Gavin Zhang <zxb_worky@163.com>
2023-03-24 11:05:31 +0800

    引入-Nacos注册中心及依赖

---
+++ armagin-server/src/main/java/cc/forim/armagin/server/infra/config/NacosConfig.java
@@ -0,0 +1,15 @@
+package cc.forim.armagin.server.infra.config;
+
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Gavin Zhang
+ * @version V1.0
+ * @since 2023/3/24 10:52
+ */
+
+@EnableDiscoveryClient
+@Configuration
+public class NacosConfig {
+}
