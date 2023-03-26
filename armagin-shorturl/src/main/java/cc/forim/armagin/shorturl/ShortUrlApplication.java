e7bb293e10968d15ba8ae070a407668966e0cbb3
Gavin Zhang <zxb_worky@163.com>
2023-03-26 20:45:38 +0800

    增加-shortUrl的基础配置

---
+++ armagin-shorturl/src/main/java/cc/forim/armagin/shorturl/ShortUrlApplication.java
@@ -0,0 +1,18 @@
+package cc.forim.armagin.shorturl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Gavin Zhang
+ * @version V1.0
+ * @since 2023/3/26 14:26
+ */
+
+@SpringBootApplication
+public class ShortUrlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShortUrlApplication.class, args);
+    }
+}
