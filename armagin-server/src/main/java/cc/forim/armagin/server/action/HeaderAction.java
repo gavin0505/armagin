489f36687ca2bdbf133fe0a35c4783160dcc6d41
Gavin Zhang <zxb_worky@163.com>
2023-04-09 13:52:40 +0800

    修改-cookie判空

--- armagin-server/src/main/java/cc/forim/armagin/server/action/HeaderAction.java
+++ armagin-server/src/main/java/cc/forim/armagin/server/action/HeaderAction.java
@@ -5,6 +5,7 @@ import cc.forim.armagin.server.infra.enums.TransformEnum;
 import cc.forim.armagin.server.infra.utils.IpUtil;
 import cc.forim.armagin.server.pipeline.BusinessProcess;
 import cc.forim.armagin.server.pipeline.TransformContext;
+import cn.hutool.core.util.StrUtil;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
@@ -21,12 +22,17 @@ import org.springframework.stereotype.Component;
 @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class HeaderAction implements BusinessProcess {
 
+    private static final String EMPTY = "";
     @Override
     public void process(TransformContext context) {
         try {
             // 获取User-Agent和Cookie并添加到上下文参数
             String ua = context.getHeader(HttpHeaderEnum.UA.getValue());
             String cookie = context.getHeader(HttpHeaderEnum.COOKIES.getValue());
+
+            if(StrUtil.hasBlank(cookie)) {
+                cookie = EMPTY;
+            }
             context.setParam(TransformEnum.UA.getValue(), ua);
             context.setParam(TransformEnum.COOKIE.getValue(), cookie);
 
