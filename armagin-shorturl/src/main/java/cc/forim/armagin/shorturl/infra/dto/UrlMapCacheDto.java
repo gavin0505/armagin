3885eef4886b7c975ef0bb9809716e09bf98026c
Gavin Zhang <zxb_worky@163.com>
2023-04-18 18:34:26 +0800

    增加-shortUrl-内部流转UrlMap -> Cache

---
+++ armagin-shorturl/src/main/java/cc/forim/armagin/shorturl/infra/dto/UrlMapCacheDto.java
@@ -0,0 +1,28 @@
+package cc.forim.armagin.shorturl.infra.dto;
+
+import lombok.Data;
+
+
+/**
+ * 内部流转UrlMap -> Cache
+ *
+ * @author Gavin Zhang
+ * @version V1.0
+ * @since 2023/4/18 16:50
+ */
+
+@Data
+public class UrlMapCacheDto {
+
+    private Long id;
+
+    private String shortUrl;
+
+    private String longUrl;
+
+    private String compressionCode;
+
+    private String description;
+
+    private Boolean enable;
+}
