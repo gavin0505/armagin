087ea10b4d5907927e0ab673896c1ca82226b677
Gavin Zhang <zxb_worky@163.com>
2023-04-18 13:39:16 +0800

    增加-域名配置实体类

---
+++ armagin-shorturl/src/main/java/cc/forim/armagin/shorturl/infra/entity/DomainConf.java
@@ -0,0 +1,47 @@
+package cc.forim.armagin.shorturl.infra.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.*;
+
+import java.time.OffsetDateTime;
+
+/**
+ * 域名配置实体类
+ *
+ * @author Gavin Zhang
+ * @version V1.0
+ * @since 2023/4/18 13:36
+ */
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Builder
+@TableName("domain_conf")
+public class DomainConf {
+
+    @TableId(type = IdType.AUTO)
+    private Long id;
+
+    private String domainValue;
+
+    private String protocol;
+
+    private Integer domainStatus;
+
+    private OffsetDateTime createTime;
+
+    private OffsetDateTime editTime;
+
+    private String creator;
+
+    private String editor;
+
+    private Long version;
+
+    private Integer deleted;
+
+}
