8c69ab89cdd8563035a383fbbd5366fa4189f223
Gavin Zhang <zxb_worky@163.com>
2023-04-18 14:13:20 +0800

    增加-shortUrl-域名配置Mapper

---
+++ armagin-shorturl/src/main/java/cc/forim/armagin/shorturl/dao/DomainConfMapper.java
@@ -0,0 +1,26 @@
+package cc.forim.armagin.shorturl.dao;
+
+import cc.forim.armagin.shorturl.infra.entity.DomainConf;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+/**
+ * 短链配置映射DB处理
+ *
+ * @author Gavin Zhang
+ * @version V1.0
+ * @since 2023/4/18 13:40
+ */
+
+@Mapper
+public interface DomainConfMapper extends BaseMapper<DomainConf> {
+
+    /**
+     * 查找短链域名是否存在
+     *
+     * @param domain 需要判断的域名
+     * @return 结果
+     */
+    DomainConf selectByDomain(@Param("domain") String domain);
+}
