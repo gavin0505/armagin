2a587cdcec8d59409daae4f157b338cf1369c715
Gavin Zhang <zxb_worky@163.com>
2023-04-19 14:31:56 +0800

    增加-server-内部流转UrlMap

---
+++ armagin-server/src/main/java/cc/forim/armagin/server/dto/UrlMapCacheDto.java
@@ -0,0 +1,29 @@
+package cc.forim.armagin.server.dto;
+
+import lombok.Data;
+
+/**
+ * 内部流转UrlMap -> Cache
+ *
+ * @author Gavin Zhang
+ * @version V1.0
+ * @since 2023/4/18 16:50
+ */
+
+@Data
+public class UrlMapCacheDto {
+
+    private Long id;
+
+    private String shortUrl;
+
+    private String longUrl;
+
+    private String compressionCode;
+
+    private String bizType;
+
+    private String description;
+
+    private Boolean enable;
+}
