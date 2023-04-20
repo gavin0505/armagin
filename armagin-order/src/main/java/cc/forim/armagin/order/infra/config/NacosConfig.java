e687434c916632386eb9c7013674db8925e724d2
Gavin Zhang <zxb_worky@163.com>
2023-04-20 20:31:43 +0800

    增加-oder订单模块

---
+++ armagin-order/src/main/java/cc/forim/armagin/order/infra/config/NacosConfig.java
@@ -0,0 +1,14 @@
+package cc.forim.armagin.order.infra.config;
+
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Gavin Zhang
+ * @version V1.0
+ * @since 2023/4/12 00:00
+ */
+@EnableDiscoveryClient
+@Configuration
+public class NacosConfig {
+}
