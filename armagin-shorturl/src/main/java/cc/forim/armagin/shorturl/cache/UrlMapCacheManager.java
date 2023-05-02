b23c02919df1e2a1c85a07f3685b3ab209de8201
Gavin Zhang <zxb_worky@163.com>
2023-05-02 23:59:23 +0800

    修改-shortUrl-创建短链时在redis延迟队列设置expire

--- armagin-shorturl/src/main/java/cc/forim/armagin/shorturl/cache/UrlMapCacheManager.java
+++ armagin-shorturl/src/main/java/cc/forim/armagin/shorturl/cache/UrlMapCacheManager.java
@@ -5,11 +5,16 @@ import cc.forim.armagin.shorturl.infra.dto.UrlMapCacheDto;
 import cc.forim.armagin.shorturl.infra.entity.UrlMap;
 import cc.forim.armagin.shorturl.infra.enums.CacheKey;
 import cc.forim.armagin.shorturl.infra.enums.UrlMapStatus;
+import cn.hutool.core.date.LocalDateTimeUtil;
 import cn.hutool.core.util.ObjectUtil;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.function.Function;
+
+import static cc.forim.armagin.shorturl.infra.enums.CommonConstant.*;
 
 /**
  * UrlMap缓存管理
@@ -21,9 +26,11 @@ import java.util.function.Function;
 
 @Component
 public class UrlMapCacheManager {
-    private static final String HASH = "hash";
 
-    private static final String COLON = ":";
+    /**
+     * 时间戳（秒）：2023-01-01 00:00:00
+     */
+    private final static long START_TIME = 1672502400L;
 
     @Resource(name = "redisUtil")
     private RedisUtil redisUtil;
@@ -42,6 +49,16 @@ public class UrlMapCacheManager {
     private void refreshUrlMapCache(UrlMapCacheDto dto) {
         // 格式：armagin:server:access:code:业务类型:hash
         redisUtil.hSet(CacheKey.ACCESS_CODE_HASH_PREFIX.getKey() + dto.getBizType() + COLON + HASH, dto.getCompressionCode(), dto);
+
+        // LocalDateTime转换
+        LocalDateTime localDateTime = LocalDateTimeUtil.of(dto.getExpireTime());
+
+        // 计算过期时长
+        Double score = Double.parseDouble(
+                String.valueOf(localDateTime.atZone(ZoneId.systemDefault()).toInstant().getEpochSecond() - START_TIME)
+        );
+        // Redis模拟延迟队列，加入对应hash的field的过期时间，格式：armagin:server:expire:access:code:业务类型:zset
+        redisUtil.zAdd(CacheKey.EXPIRE_ACCESS_CODE_ZSET_PREFIX.getKey() + dto.getBizType() + COLON + ZSET, dto.getCompressionCode(), score);
     }
 
     /**
@@ -57,6 +74,7 @@ public class UrlMapCacheManager {
         urlMapCacheDto.setCompressionCode(urlMap.getCompressionCode());
         urlMapCacheDto.setBizType(urlMap.getBizType());
         urlMapCacheDto.setEnable(UrlMapStatus.AVAILABLE.getValue().equals(urlMap.getUrlStatus()));
+        urlMapCacheDto.setExpireTime(urlMap.getExpireTime());
 
         return urlMapCacheDto;
     };
