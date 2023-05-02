73ef42f3c8b514599ace3a0785e491c5a0d61ede
Gavin Zhang <zxb_worky@163.com>
2023-05-02 23:58:39 +0800

    增加-shortUrl-增加普通常量枚举

---
+++ armagin-shorturl/src/main/java/cc/forim/armagin/shorturl/infra/enums/CommonConstant.java
@@ -0,0 +1,23 @@
+package cc.forim.armagin.shorturl.infra.enums;
+
+/**
+ * 普通常量枚举
+ *
+ * @author Gavin Zhang
+ * @version V1.0
+ * @since 2023/5/2 22:34
+ */
+
+public class CommonConstant {
+
+    public static final String HASH = "hash";
+
+    public static final String ZSET = "zset";
+
+    public static final String COLON = ":";
+
+    /**
+     * 时间戳（秒）：2023-01-01 00:00:00
+     */
+    public final static long START_TIME = 1672502400L;
+}
