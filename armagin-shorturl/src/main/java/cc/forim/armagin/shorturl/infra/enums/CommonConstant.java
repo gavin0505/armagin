f174aa0c04df47121f2c9a7b740ffed92547a501
Gavin Zhang <zxb_worky@163.com>
2023-05-04 20:06:26 +0800

    修改-shortUrl-引入无限时间以解决长效链接在redis无法缓存的问题

--- armagin-shorturl/src/main/java/cc/forim/armagin/shorturl/infra/enums/CommonConstant.java
+++ armagin-shorturl/src/main/java/cc/forim/armagin/shorturl/infra/enums/CommonConstant.java
@@ -16,6 +16,8 @@ public class CommonConstant {
 
     public static final String COLON = ":";
 
+    public static final String INFINITY_DATE = "2022-12-31 23:59:59";
+
     /**
      * 时间戳（秒）：2023-01-01 00:00:00
      */
