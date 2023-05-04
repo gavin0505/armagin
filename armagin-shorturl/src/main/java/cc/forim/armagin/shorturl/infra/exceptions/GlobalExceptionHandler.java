afd72d103fd5d63fdfb18b0269e1976183535162
Gavin Zhang <zxb_worky@163.com>
2023-05-04 15:38:18 +0800

    增加-shortUrl-全局异常处理器

---
+++ armagin-shorturl/src/main/java/cc/forim/armagin/shorturl/infra/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,28 @@
+package cc.forim.armagin.shorturl.infra.exceptions;
+
+import cc.forim.armagin.common.ResultVo;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * 全局异常处理器
+ *
+ * @author Gavin Zhang
+ * @version V1.0
+ * @since 2023/5/4 14:20
+ */
+
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+    /**
+     * IllegalArgumentException处理
+     */
+    @ExceptionHandler(value = IllegalArgumentException.class)
+    public ResultVo<String> illegalArgumentExceptionHandler(IllegalArgumentException e) {
+        log.info(e.getMessage());
+        return ResultVo.error(e.getMessage());
+    }
+}
