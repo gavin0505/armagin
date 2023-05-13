f343899c3745e6abeb9052453ab6a1334b9f279d
Gavin Zhang <zxb_worky@163.com>
2023-05-13 22:25:09 +0800

    增加-gateway-增加网关微服务

---
+++ armagin-gateway/src/main/java/cc/forim/armagin/gateway/GateWayApplication.java
@@ -0,0 +1,20 @@
+package cc.forim.armagin.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 网关启动类
+ *
+ * @author Gavin Zhang
+ * @version V1.0
+ * @since 2023/5/11 23:08
+ */
+
+@SpringBootApplication
+public class GateWayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayApplication.class, args);
+    }
+}
