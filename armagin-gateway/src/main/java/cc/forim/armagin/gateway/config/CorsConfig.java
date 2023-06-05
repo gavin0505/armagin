5459a0f893a3820b25b797ad3c5ddd364461512f
Gavin Zhang <zxb_worky@163.com>
2023-06-05 14:08:38 +0800

    增加-gateway-跨域配置

---
+++ armagin-gateway/src/main/java/cc/forim/armagin/gateway/config/CorsConfig.java
@@ -0,0 +1,29 @@
+package cc.forim.armagin.gateway.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+/**
+ * 跨域配置
+ *
+ * @author Gavin Zhang
+ * @version V1.0
+ * @since 2023/3/22 14:25
+ */
+@Configuration
+public class CorsConfig implements WebFluxConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        // 允许访问路径
+        registry.addMapping("/**")
+                // 允许跨域访问的源
+                .allowedOrigins("*")
+                // 允许跨域访问的方法
+                .allowedMethods("POST", "GET", "DELETE", "PUT", "OPTIONS")
+                .allowedHeaders("*")
+                // 预检间隔时间
+                .maxAge(10000);
+    }
+}
