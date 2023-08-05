3d605dae32e5b2e61f8404a09afc4f551773f02f
Gavin Zhang <zxb_worky@163.com>
2023-08-05 12:16:55 +0800

    增加-common-增加RedisUtil功能

--- armagin-common/src/main/java/cc/forim/armagin/common/utils/RedisUtil.java
+++ armagin-common/src/main/java/cc/forim/armagin/common/utils/RedisUtil.java
@@ -134,9 +134,9 @@ public class RedisUtil {
     /**
      * 普通缓存放入并设置时间
      *
-     * @param key   键
-     * @param value 值
-     * @param time  时间(秒) time要大于0 如果time小于等于0 将设置无限期
+     * @param key        键
+     * @param value      值
+     * @param expireTime 时间(秒) time要大于0 如果time小于等于0 将设置无限期
      * @return true成功 false 失败
      */
     public Boolean set(String key, Object value, long expireTime) {
@@ -199,15 +199,30 @@ public class RedisUtil {
     /**
      * 向一张hash表中放入数据,如果不存在将创建
      *
-     * @param key   键
-     * @param item  项
-     * @param value 值
+     * @param key     hash表名
+     * @param hashKey 项
+     * @param value   值
      */
-    public void hSet(String key, String item, Object value) {
+    public void hSet(String key, String hashKey, Object value) {
         try {
-            redisTemplate.opsForHash().put(key, item, value);
+            redisTemplate.opsForHash().put(key, hashKey, value);
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    /**
+     * 获取hash表中的一行数据
+     *
+     * @param key     键
+     * @param hashKey 项
+     */
+    public Object hGet(String key, Object hashKey) {
+        try {
+            return redisTemplate.opsForHash().get(key, hashKey);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
         }
     }
 
@@ -349,6 +364,23 @@ public class RedisUtil {
         } catch (Exception e) {
             e.printStackTrace();
             return 0L;
+        }
+    }
+
+    /**
+     * 增加zSet某项的Score
+     *
+     * @param key    键
+     * @param item   项
+     * @param scores 增加的score
+     * @return 修改后的score值
+     */
+    public Double zSetIncrScore(String key, String item, Double scores) {
+        try {
+            return redisTemplate.opsForZSet().incrementScore(key, item, scores);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return 0.0;
         }
     }
 }
